e264df7e9ba78e32b4e3ad243d5446ec18dbb197
joshlive77 <jozulett@gmail.com>
2019-09-19 13:52:09 -0400

    editado por: Josh Zulett el: 19/09/2019 a horas: 13:52

--- lib/shortcodes/lista-galerias/templates/galerias.php
+++ lib/shortcodes/lista-galerias/templates/galerias.php
@@ -2,22 +2,22 @@
     <div class="galeria" style="display: block;">
         <div class="contenedor">
             <div class="imagen">
-                <img src="http://iicca.test/wp-content/uploads/2019/09/bannerinter.jpg">
+                <img src="https://iicca.edu.bo/wp-content/uploads/2019/09/N66.jpg">
             </div>
             <div class="contenido">
                 <div class="titulo">
-                    <a href="">
+                    <a href="https://iicca.edu.bo/galeria-prueba/">
                         <h3>TITULO</h3>
                     </a>
                 </div>
                 <div class="etiquetas">
-                    <a href="">
+                    <a href="https://iicca.edu.bo/galerias-lista-prueba/">
                         <span>tag1</span>
                     </a>
-                    <a href="">
+                    <a href="https://iicca.edu.bo/galerias-lista-prueba/">
                         <span>tag1</span>
                     </a>
-                    <a href="">
+                    <a href="https://iicca.edu.bo/galerias-lista-prueba/">
                         <span>tag1</span>
                     </a>
                 </div>
@@ -30,22 +30,22 @@
     <div class="galeria" style="display: block;">
         <div class="contenedor">
             <div class="imagen">
-                <img src="http://iicca.test/wp-content/uploads/2019/09/bannerinter.jpg">
+                <img src="https://iicca.edu.bo/wp-content/uploads/2019/09/comex0.jpg">
             </div>
             <div class="contenido">
                 <div class="titulo">
-                    <a href="">
+                    <a href="https://iicca.edu.bo/galeria-prueba/">
                         <h3>TITULO</h3>
                     </a>
                 </div>
                 <div class="etiquetas">
-                    <a href="">
+                    <a href="https://iicca.edu.bo/galerias-lista-prueba/">
                         <span>tag1</span>
                     </a>
-                    <a href="">
+                    <a href="https://iicca.edu.bo/galerias-lista-prueba/">
                         <span>tag1</span>
                     </a>
-                    <a href="">
+                    <a href="https://iicca.edu.bo/galerias-lista-prueba/">
                         <span>tag1</span>
                     </a>
                 </div>
@@ -58,22 +58,22 @@
     <div class="galeria" style="display: block;">
         <div class="contenedor">
             <div class="imagen">
-                <img src="http://iicca.test/wp-content/uploads/2019/09/bannerinter.jpg">
+                <img src="https://iicca.edu.bo/wp-content/uploads/2017/10/N2.jpg">
             </div>
             <div class="contenido">
                 <div class="titulo">
-                    <a href="">
+                    <a href="https://iicca.edu.bo/galeria-prueba/">
                         <h3>TITULO</h3>
                     </a>
                 </div>
                 <div class="etiquetas">
-                    <a href="">
+                    <a href="https://iicca.edu.bo/galerias-lista-prueba/">
                         <span>tag1</span>
                     </a>
-                    <a href="">
+                    <a href="https://iicca.edu.bo/galerias-lista-prueba/">
                         <span>tag1</span>
                     </a>
-                    <a href="">
+                    <a href="https://iicca.edu.bo/galerias-lista-prueba/">
                         <span>tag1</span>
                     </a>
                 </div>
@@ -86,22 +86,162 @@
     <div class="galeria" style="display: block;">
         <div class="contenedor">
             <div class="imagen">
-                <img src="http://iicca.test/wp-content/uploads/2019/09/bannerinter.jpg">
+                <img src="https://iicca.edu.bo/wp-content/uploads/2019/09/N66.jpg">
             </div>
             <div class="contenido">
                 <div class="titulo">
-                    <a href="">
+                    <a href="https://iicca.edu.bo/galeria-prueba/">
                         <h3>TITULO</h3>
                     </a>
                 </div>
                 <div class="etiquetas">
-                    <a href="">
+                    <a href="https://iicca.edu.bo/galerias-lista-prueba/">
                         <span>tag1</span>
                     </a>
-                    <a href="">
+                    <a href="https://iicca.edu.bo/galerias-lista-prueba/">
                         <span>tag1</span>
                     </a>
-                    <a href="">
+                    <a href="https://iicca.edu.bo/galerias-lista-prueba/">
+                        <span>tag1</span>
+                    </a>
+                </div>
+                <div class="descripcion">
+                    <P>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae neque eius incidunt assumenda nesciunt eum explicabo laudantium, soluta quaerat libero.</P>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="galeria" style="display: block;">
+        <div class="contenedor">
+            <div class="imagen">
+                <img src="https://iicca.edu.bo/wp-content/uploads/2019/09/comex0.jpg">
+            </div>
+            <div class="contenido">
+                <div class="titulo">
+                    <a href="https://iicca.edu.bo/galeria-prueba/">
+                        <h3>TITULO</h3>
+                    </a>
+                </div>
+                <div class="etiquetas">
+                    <a href="https://iicca.edu.bo/galerias-lista-prueba/">
+                        <span>tag1</span>
+                    </a>
+                    <a href="https://iicca.edu.bo/galerias-lista-prueba/">
+                        <span>tag1</span>
+                    </a>
+                    <a href="https://iicca.edu.bo/galerias-lista-prueba/">
+                        <span>tag1</span>
+                    </a>
+                </div>
+                <div class="descripcion">
+                    <P>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae neque eius incidunt assumenda nesciunt eum explicabo laudantium, soluta quaerat libero.</P>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="galeria" style="display: block;">
+        <div class="contenedor">
+            <div class="imagen">
+                <img src="https://iicca.edu.bo/wp-content/uploads/2017/10/N2.jpg">
+            </div>
+            <div class="contenido">
+                <div class="titulo">
+                    <a href="https://iicca.edu.bo/galeria-prueba/">
+                        <h3>TITULO</h3>
+                    </a>
+                </div>
+                <div class="etiquetas">
+                    <a href="https://iicca.edu.bo/galerias-lista-prueba/">
+                        <span>tag1</span>
+                    </a>
+                    <a href="https://iicca.edu.bo/galerias-lista-prueba/">
+                        <span>tag1</span>
+                    </a>
+                    <a href="https://iicca.edu.bo/galerias-lista-prueba/">
+                        <span>tag1</span>
+                    </a>
+                </div>
+                <div class="descripcion">
+                    <P>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae neque eius incidunt assumenda nesciunt eum explicabo laudantium, soluta quaerat libero.</P>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="galeria" style="display: block;">
+        <div class="contenedor">
+            <div class="imagen">
+                <img src="https://iicca.edu.bo/wp-content/uploads/2019/09/N66.jpg">
+            </div>
+            <div class="contenido">
+                <div class="titulo">
+                    <a href="https://iicca.edu.bo/galeria-prueba/">
+                        <h3>TITULO</h3>
+                    </a>
+                </div>
+                <div class="etiquetas">
+                    <a href="https://iicca.edu.bo/galerias-lista-prueba/">
+                        <span>tag1</span>
+                    </a>
+                    <a href="https://iicca.edu.bo/galerias-lista-prueba/">
+                        <span>tag1</span>
+                    </a>
+                    <a href="https://iicca.edu.bo/galerias-lista-prueba/">
+                        <span>tag1</span>
+                    </a>
+                </div>
+                <div class="descripcion">
+                    <P>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae neque eius incidunt assumenda nesciunt eum explicabo laudantium, soluta quaerat libero.</P>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="galeria" style="display: block;">
+        <div class="contenedor">
+            <div class="imagen">
+                <img src="https://iicca.edu.bo/wp-content/uploads/2019/09/comex0.jpg">
+            </div>
+            <div class="contenido">
+                <div class="titulo">
+                    <a href="https://iicca.edu.bo/galeria-prueba/">
+                        <h3>TITULO</h3>
+                    </a>
+                </div>
+                <div class="etiquetas">
+                    <a href="https://iicca.edu.bo/galerias-lista-prueba/">
+                        <span>tag1</span>
+                    </a>
+                    <a href="https://iicca.edu.bo/galerias-lista-prueba/">
+                        <span>tag1</span>
+                    </a>
+                    <a href="https://iicca.edu.bo/galerias-lista-prueba/">
+                        <span>tag1</span>
+                    </a>
+                </div>
+                <div class="descripcion">
+                    <P>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae neque eius incidunt assumenda nesciunt eum explicabo laudantium, soluta quaerat libero.</P>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="galeria" style="display: block;">
+        <div class="contenedor">
+            <div class="imagen">
+                <img src="https://iicca.edu.bo/wp-content/uploads/2017/10/N2.jpg">
+            </div>
+            <div class="contenido">
+                <div class="titulo">
+                    <a href="https://iicca.edu.bo/galeria-prueba/">
+                        <h3>TITULO</h3>
+                    </a>
+                </div>
+                <div class="etiquetas">
+                    <a href="https://iicca.edu.bo/galerias-lista-prueba/">
+                        <span>tag1</span>
+                    </a>
+                    <a href="https://iicca.edu.bo/galerias-lista-prueba/">
+                        <span>tag1</span>
+                    </a>
+                    <a href="https://iicca.edu.bo/galerias-lista-prueba/">
                         <span>tag1</span>
                     </a>
                 </div>
